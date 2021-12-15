df7f28d6da07256449d6c4da4265085502094ee4
Erik Brinkman <erik.brinkman@gmail.com>
2021-12-14 20:56:34 -0500

    build

--- docs/_next/static/-qHP_eDnZ2uEKOOfQ6vdb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-389774ab5c92bcad1b4d.js","static/chunks/pages/index-84922f48ffe6f445b0ae.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

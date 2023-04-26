ed3b132c06dff622f32d3d5de933ed6dc1d678bc
Erik Brinkman <erik.brinkman@gmail.com>
2023-04-26 00:26:27 -0400

    update

--- docs/_next/static/chunks/pages/_error-8b1568e18035cde8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{8947:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(6112)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8947)}),_N_E=n.O()}]);
---
+++ docs/_next/static/r1XmApHeALxryEr9KeViY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76890a9e-773118581973eb98.js","static/chunks/212-12604ad44dfcdd24.js","static/chunks/pages/index-b35185bb3cb352b9.js"],"/_error":["static/chunks/pages/_error-eab4ded74d156b37.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

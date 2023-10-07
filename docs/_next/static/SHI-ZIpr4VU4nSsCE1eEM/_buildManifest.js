d2891adf2073e750871100000bbd1b6f7ec59cd4
Erik Brinkman <erik.brinkman@gmail.com>
2023-10-06 22:23:35 -0400

    upgrade and release

---
+++ docs/_next/static/SHI-ZIpr4VU4nSsCE1eEM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c77e026a5fd97096.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
